7aabc2fdadf00a03ac788acc45ba41c5498c1c92
Muqeet-Ryan <muqeetryan@outlook.com>
2026-06-12 17:51:20 +0600

    auth,jwt,validations, signup/in/out configured

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+import authRoutes from '#routes/auth.routes.js';
+import { timestamp } from 'drizzle-orm/gel-core';
+
 const app = express();
 
 app.use(helmet());
@@ -18,12 +21,24 @@ app.use(
   })
 );
 
-
-
-
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisions!');
+  res.status(200).json('Hello from Acquision API');
+});
+
+
+app.get('/api', (req, res) => {
   res.status(200).json('Acquision API is running');
 });
+
+app.get('/health', (req,res) => {
+  res.status(200).json({status: 'ok', timestamp: new Date().toISOString(), uptime: process.uptime() });
+});
+
+app.use('/api/auth', authRoutes);
+
+
+
+
 
 export default app;
