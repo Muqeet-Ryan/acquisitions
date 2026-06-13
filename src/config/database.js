2a63e04668506c9b4c4babccb5f6b93bec793b73
Muqeet-Ryan <muqeetryan@outlook.com>
2026-06-13 20:03:32 +0600

    docker_setup, local neon db, scripting to build images and containerized app

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,16 @@
 import 'dotenv/config';
-import { neon } from '@neondatabase/serverless';
+
+import { neon, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-export const sql = neon(process.env.DATABASE_URL);
+if (process.env.NODE_ENV === 'development') {
+  neonConfig.fetchEndpoint = 'http://neon-local:5432/sql';
+  neonConfig.useSecureWebSocket = false;
+  neonConfig.poolQueryViaFetch = true;
+}
 
-export const db = drizzle(sql);
+const sql = neon(process.env.DATABASE_URL);
+
+const db = drizzle(sql);
+
+export { db, sql };
